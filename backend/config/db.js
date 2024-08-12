475b7515adc375890ccb565bb4ffda4cfa98fecf
Simran_Yadav <simranyadav464@gmail.com>
2024-08-12 19:13:54 +0530

    Modified the code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,27 @@
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  const client = new MongoClient(uri, {
+    tls: true,
+    tlsInsecure: true,
+
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
+  } finally {
+    await client.close();
   }
 };
 
